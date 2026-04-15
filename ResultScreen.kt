3572cde085adba2720e4b3b573cfd8dd8be4ffd7
DeniseMaldonadoG <128629396+DeniseMaldonadoG@users.noreply.github.com>
2026-04-14 20:21:16 -0600

    Update ResultScreen.kt

--- ResultScreen.kt
+++ ResultScreen.kt
@@ -2,7 +2,6 @@ package com.pdm0126.taller1_00133523.ui.theme
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
