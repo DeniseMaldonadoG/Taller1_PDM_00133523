babfa0adce3203711ed750e136aa54a0533b6316
DeniseMaldonadoG <128629396+DeniseMaldonadoG@users.noreply.github.com>
2026-04-20 22:56:14 -0600

    Implement state saving for QuizState
    Added a companion object with a listSaver for QuizState to manage state saving and restoration.

--- QuizState.kt
+++ QuizState.kt
@@ -1,4 +1,5 @@
 package com.pdm0126.taller1_00133523.ui.theme
+import androidx.compose.runtime.saveable.listSaver
 
 data class QuizState(
     val currentQuestionIndex: Int = 0,
@@ -9,4 +10,35 @@ data class QuizState(
     val selectedAnswerIndex: Int? = null,
     val showFunFact: Boolean = false,
     val isQuizFinished: Boolean = false
-)
+){
+    companion object {
+
+        val Saver = listSaver(
+            save = {
+                listOf(
+                    it.currentQuestionIndex,
+                    it.score,
+                    it.totalQuestions,
+                    it.isQuizStarted,
+                    it.isAnswered,
+                    it.selectedAnswerIndex,
+                    it.showFunFact,
+                    it.isQuizFinished
+                )
+            },
+
+            restore = {
+                QuizState(
+                    currentQuestionIndex = it[0] as Int,
+                    score = it[1] as Int,
+                    totalQuestions = it[2] as Int,
+                    isQuizStarted = it[3] as Boolean,
+                    isAnswered = it[4] as Boolean,
+                    selectedAnswerIndex = it[5] as Int?,
+                    showFunFact = it[6] as Boolean,
+                    isQuizFinished = it[7] as Boolean
+                )
+            }
+        )
+    }
+}
